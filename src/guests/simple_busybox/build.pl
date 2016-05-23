67e987b6da8609ee3d95b2ab0c10e4143fbbb53e
Noah Evans <noah.evans@gmail.com>
2016-05-23 17:55:06 -0400

    steal pmi.h from mpi

--- src/guests/simple_busybox/build.pl
+++ src/guests/simple_busybox/build.pl
@@ -424,6 +424,7 @@ if ($program_args{build_ompi}) {
 	# This means we need to be root to do a make install and will possibly screw up the host.
 	# We should really be using chroot or something better.
 	#system ("LD_LIBRARY_PATH=$BASEDIR/$SRCDIR/slurm-install/lib ./configure --prefix=/opt/$ompi{basename} --disable-shared --enable-static --with-verbs=yes") == 0
+	system("cp opal/mca/pmix/pmix120/pmix/include/pmi.h $BASEDIR/$SRCDIR/pisces/hobbes/libhobbes/include") == 0 or die "couldn't copy pmi.h";
 	if(! -e "configure" ){
 		system("./autogen.pl") == 0 || die "couldn't generate configure for openmpi";
 	}
